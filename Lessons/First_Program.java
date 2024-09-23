9274f7ac674008e00c92604e07e50a42533fe00c
consolejockey <gabdi1993@gmail.com>
2024-09-22 23:25:47 -0400

    Create First_Program.java

---
+++ Lessons/First_Program.java
@@ -0,0 +1,20 @@
+//A simple Java program appears below.
+/*
+
+A program starts in main(), executing the statements within main's braces { }, one at a time.
+Each statement typically appears alone on a line and ends with a semicolon, as English sentences end with a period.
+The int wage statement creates an integer variable named wage. The wage = 20 statement assigns wage with 20.
+The print and println statements output various values.
+
+*/
+public class First_Program {                       
+
+  public static void main (String [] args) {
+      int wage;
+
+      wage = 20;                        
+
+      System.out.print("Salary is ");       
+      System.out.println(wage * 40 * 52);   
+  }
+}
